5d722837daf1aa853b353dee0ac811270b618fb6
JonoMitter <jonomitter@outlook.com>
2022-03-27 17:17:40 +1100

    added Splash component

---
+++ src/components/Splash.tsx
@@ -0,0 +1,10 @@
+function Splash() {
+  return (
+    <div className="App">
+      <h1>Jonathon Mitterbacher</h1>
+      <h2><b>Software Engineer</b></h2>
+    </div>
+  );
+}
+
+export default Splash;
